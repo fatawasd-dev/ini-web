53d3c346b03238d0a0ecd8d5b9fa9fe2cc9a1cfb
Fahmi Agung Tajul Abidin <fata.22032001@gmail.com>
2019-12-07 00:21:18 +0700

    Updates

---
+++ precache-manifest.89fb6eb84362d314da920c3bd111aa7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "379b2918be059885772619adf00218b3",
+    "url": "/ini-web/index.html"
+  },
+  {
+    "revision": "3f82a01b327727af04c2",
+    "url": "/ini-web/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "eee5bb5c626327f36d48",
+    "url": "/ini-web/static/js/2.ba0ee66d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ini-web/static/js/2.ba0ee66d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f82a01b327727af04c2",
+    "url": "/ini-web/static/js/main.3ba682b3.chunk.js"
+  },
+  {
+    "revision": "d5b205afd99da5eef9aa",
+    "url": "/ini-web/static/js/runtime-main.b8b022b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ini-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
